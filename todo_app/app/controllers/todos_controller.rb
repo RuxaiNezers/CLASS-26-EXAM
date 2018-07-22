e22a2955d8ad90c6e185bc6f3549c56d3853eb06
RuxaiNezers <alexis.neira.cruz@gmail.com>
2018-07-22 15:57:47 -0400

    part one, until migration, fixed

--- todo_app/app/controllers/todos_controller.rb
+++ todo_app/app/controllers/todos_controller.rb
@@ -1,13 +1,2 @@
 class TodosController < ApplicationController
-
-	private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_todo
-      @todo = Todos.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def todo_params
-      params.require(:todo).permit(:description, :completed)
-    end
 end
--- todo_app/db/schema.rb
+++ todo_app/db/schema.rb
@@ -10,11 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_22_002000) do
+ActiveRecord::Schema.define(version: 2018_07_22_193617) do
 
   create_table "todos", force: :cascade do |t|
-    t.string "description"
-    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
