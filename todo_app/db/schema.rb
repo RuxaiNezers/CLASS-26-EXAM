f2ff7b2f62c3752165f513760054abe0a423b10c
RuxaiNezers <alexis.neira.cruz@gmail.com>
2018-07-22 18:10:32 -0400

    index completed

--- todo_app/db/schema.rb
+++ todo_app/db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_22_193617) do
+ActiveRecord::Schema.define(version: 2018_07_22_203010) do
 
   create_table "todos", force: :cascade do |t|
+    t.string "description"
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
