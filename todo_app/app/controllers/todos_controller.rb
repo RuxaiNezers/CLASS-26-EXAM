76c616570591e949059d0e7ab7cf08005b6a3f42
RuxaiNezers <alexis.neira.cruz@gmail.com>
2018-07-23 22:40:28 -0400

    new part completed and operative

--- todo_app/app/controllers/todos_controller.rb
+++ todo_app/app/controllers/todos_controller.rb
@@ -7,5 +7,16 @@ class TodosController < ApplicationController
 	def new
 		@todo =Todo.new
 	end
+
+	def create
+		@todo = Todo.new(post_params)
+		@todo.save
+		redirect_to todos_path
+	end
+
+	private
+		def post_params
+			params.require(:todo).permit(:description, :completed)
+		end
 	
 end
